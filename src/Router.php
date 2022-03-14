6d39119123bb5f140aec11effb4700c34552f420
Loumzer <lucasmuzart@hotmail.fr>
2022-03-14 16:36:52 +0100

    test

--- src/Router.php
+++ src/Router.php
@@ -14,15 +14,9 @@ class Router{
     $view = new View($this);
     $storage = new Database();
     $controller = new Controller($view, $storage);
-<<<<<<< HEAD
-=======
-    /*if(isset($_GET["portfeuille"]) && $_SESSION["connexion"]!=null){
-      $console.
-    }*/
     if(isset($_GET["calendar"])){
       $view->makeCalendarPage();
     }
->>>>>>> e74ddff1b47f9d407966d2da5fc04f2b9b340a23
     if(isset($_GET["portefeuille"]) && $_SESSION["connexion"]!=null){
       if(isset($_POST["ajout"])){
         $controller->showPortefeuille(array("action"=>"achat", "symbol"=>$_POST["symbol_action"], "nbr"=>$_POST["nbr_action"], "prix"=>$_POST["prix_action"]));
